6b570dd72bc04e17ee74d169bfb792bab86cb61d
Marcel Laverdet <marcel@laverdet.com>
2019-12-06 02:25:34 -0600

    Fix Isolate handle garbage collection
    Apparently dangling references to isolates would never get cleaned up
    without calling `dispose()` on them. Who knew!

---
+++ tests/orphaned-isolate-gc.js
@@ -0,0 +1,11 @@
+// node-args: --expose-gc
+const ivm = require('isolated-vm');
+let isolate = Array(100).fill().map(() => new ivm.Isolate());
+gc();
+let rss1 = process.memoryUsage().rss;
+isolate = null;
+gc();
+let rss2 = process.memoryUsage().rss;
+if (rss1 - rss2 > 20 * 1024 * 1024) {
+	console.log('pass');
+}
